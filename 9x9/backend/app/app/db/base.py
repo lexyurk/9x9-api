d422a54aef30719b84bf752e22c8f4e658a72a10
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-06-02 16:59:49 +0300

    Add db models to alembic

--- 9x9/backend/app/app/db/base.py
+++ 9x9/backend/app/app/db/base.py
@@ -3,3 +3,6 @@
 from app.db.base_class import Base  # noqa
 from app.db_models.user import User  # noqa
 from app.db_models.item import Item  # noqa
+from app.db_models.move import Move  # noqa
+from app.db_models.game import user_game_table  # noqa
+from app.db_models.game import Game  # noqa
