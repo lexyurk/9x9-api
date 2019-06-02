c70f7e766872f51c5a5b55bd1dd8066e04e98ac2
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-06-02 18:13:33 +0300

    Add lobby game creating

--- 9x9/backend/app/app/crud/game.py
+++ 9x9/backend/app/app/crud/game.py
@@ -1,3 +1,4 @@
+from app.core.game.game import game_manager
 from app.db.session import Session
 from app.db_models.game import Game
 from app.db_models.user import User as DBUser
@@ -6,6 +7,7 @@ from app.models.game import GameStatus
 
 def create(db_session: Session, *, creator: DBUser) -> Game:
     game = Game(players=creator, status=GameStatus.CREATED)
+    game_manager.create_game(game.id)
     db_session.add(game)
     db_session.commit()
     db_session.refresh(game)
