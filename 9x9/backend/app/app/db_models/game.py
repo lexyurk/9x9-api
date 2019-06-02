76a52d3b8a36090949122ec33b541ebaf7e381ca
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-06-02 16:59:34 +0300

    Add game models

--- 9x9/backend/app/app/db_models/game.py
+++ 9x9/backend/app/app/db_models/game.py
@@ -1,18 +1,18 @@
-from sqlalchemy import Boolean, Column, Integer, DateTime, func
+from sqlalchemy import Column, Integer, Enum, DateTime, func, Table, ForeignKey
 from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
+from app.models.game import GameStatus
+
+user_game_table = Table('user_game_association', Base.metadata,
+                        Column('game_id', Integer, ForeignKey('game.id')),
+                        Column('user_id', Integer, ForeignKey('user.id'))
+                        )
 
 
 class Game(Base):
     id = Column(Integer, primary_key=True, index=True)
-    is_active = Column(Boolean(), default=True)
-    is_end = Column(Boolean(), default=False)
-    active_users = Column(Integer)
-    moves = relationship("Move",
-                         back_populates="owner",
-                         cascade="save-update, merge, delete",
-                         order_by="Move.id"
-                         )
-    players = relationship("User")
-    last_action = Column(DateTime, onupdate=func.now())
+    status = Column(Enum(GameStatus, index=True))
+    # moves = relationship("Move", back_populates="game_id")
+    players = relationship("User", secondary=user_game_table, back_populates="games")
+    last_update = Column(DateTime, onupdate=func.now())
