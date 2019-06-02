2cde0e09ab4a86c4d7b267e51a654957530755b4
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-06-02 17:00:05 +0300

    Change game crud

--- 9x9/backend/app/app/crud/game.py
+++ 9x9/backend/app/app/crud/game.py
@@ -1,14 +1,11 @@
-from fastapi.encoders import jsonable_encoder
-
 from app.db.session import Session
 from app.db_models.game import Game
-from app.models.game import GameCreate
+from app.db_models.user import User as DBUser
+from app.models.game import GameStatus
 
 
-def create(db_session: Session) -> Game:
-    new_game = GameCreate()
-    game_in_data = jsonable_encoder(new_game)
-    game = Game(**game_in_data)
+def create(db_session: Session, *, creator: DBUser) -> Game:
+    game = Game(players=creator, status=GameStatus.CREATED)
     db_session.add(game)
     db_session.commit()
     db_session.refresh(game)
