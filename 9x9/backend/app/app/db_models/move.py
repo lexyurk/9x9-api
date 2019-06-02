93a34044dc60967126a174b9761f9fd9e5dad3b4
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-06-02 16:59:25 +0300

    Update move db relationships

--- 9x9/backend/app/app/db_models/move.py
+++ 9x9/backend/app/app/db_models/move.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ARRAY
+from sqlalchemy import Column, Integer, ForeignKey, ARRAY
 from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
@@ -6,6 +6,7 @@ from app.db.base_class import Base
 
 class Move(Base):
     id = Column(Integer, primary_key=True, index=True)
-    user = relationship("User")
-    outer_field = ARRAY(Integer)
+    player_id = Column(Integer, ForeignKey("user.id"))
+    game_id = Column(Integer, ForeignKey("game.id"))
     inner_field = ARRAY(Integer)
+    outer_field = ARRAY(Integer)
