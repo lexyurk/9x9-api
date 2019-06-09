f3513bbf3642831a3395764c9cfb60bdbe81d7ca
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-06-10 00:38:37 +0300

    Add test websocket endpoint

--- 9x9/backend/app/app/api/api_v1/endpoints/game.py
+++ 9x9/backend/app/app/api/api_v1/endpoints/game.py
@@ -13,6 +13,44 @@ from app.models.game import Game
 
 router = APIRouter()
 
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost/api/v1/game/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@router.get('/test_websockets')
+def test_websockets():
+    return HTMLResponse(html)
 
 @router.post("/create", response_model=Game)
 def create_game(
