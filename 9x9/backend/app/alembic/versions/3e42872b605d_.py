f29b5238f854ef2579a511179cab388c94cba240
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-06-13 08:00:52 +0300

    Add migrations

---
+++ 9x9/backend/app/alembic/versions/3e42872b605d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3e42872b605d
+Revises: 1176c7b3c1ba
+Create Date: 2019-06-01 18:27:10.780505
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3e42872b605d'
+down_revision = '1176c7b3c1ba'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
