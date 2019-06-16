6e6e9c8a7e12ba2d950fa8f90e891bbce76df437
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-06-16 11:10:28 +0300

    Add converting to move model and back

--- 9x9/backend/app/app/crud/game.py
+++ 9x9/backend/app/app/crud/game.py
@@ -1,8 +1,11 @@
+from typing import List
+
 from app.core.game.game import game_manager
 from app.db.session import Session
 from app.db_models.game import Game
 from app.db_models.user import User as DBUser
 from app.models.game import GameStatus
+from app.models.move import MoveRequestSchema, Coordinate, Move, MoveResponseSchema
 
 
 def create(db_session: Session, *, creator: DBUser) -> Game:
@@ -12,3 +15,27 @@ def create(db_session: Session, *, creator: DBUser) -> Game:
     db_session.commit()
     db_session.refresh(game)
     return game
+
+
+def convert_to_move_model(request_move: MoveRequestSchema, user_id: int):
+    return Move(
+        player_id=user_id,
+        inner_field=_convert_to_coordinate(request_move.inner_field),
+        outer_field=_convert_to_coordinate(request_move.outer_field)
+    )
+
+
+def convert_to_response_model(move: Move) -> MoveResponseSchema:
+    return MoveResponseSchema(
+        player_id=move.player_id,
+        inner_field=_convert_coordinate_to_list(move.inner_field),
+        outer_field=_convert_coordinate_to_list(move.outer_field)
+    )
+
+
+def _convert_to_coordinate(move_coordinate: List[int]) -> Coordinate:
+    return Coordinate(x=move_coordinate[0], y=move_coordinate[1])
+
+
+def _convert_coordinate_to_list(move_coordinate: Coordinate) -> List[int]:
+    return [move_coordinate.x, move_coordinate.y]
